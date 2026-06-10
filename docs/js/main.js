937c54233b05c42d12e7cbe8b5df22c503a812e3
Shah Alam <71381609+shahalam179@users.noreply.github.com>
2026-06-10 21:42:57 +0600

    file added

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,41 +2,38 @@
 // This function controls sidebar menu (HTML or CSS)
 
 function loadMenu(type) {
-
   // Get sidebar element
   let sidebar = document.getElementById("sidebar");
   const viewBase = "views/";
 
   // If user clicks HTML button
   if (type === "html") {
-
     sidebar.innerHTML = `
       <h3>HTML Topics</h3>
 
-      <a href="${viewBase}about.html" onclick="event.preventDefault(); loadPage('${viewBase}html.about.html');">HTML Intro</a>
-      <a href="${viewBase}html-html-editor.html" onclick="event.preventDefault(); loadPage('${viewBase}html-html-editor.html');">HTML Editor</a>
-      <a href="${viewBase}html-basic.html" onclick="event.preventDefault(); loadPage('${viewBase}html-basic.html');">HTML Basic</a>
-      <a href="${viewBase}html-tags.html" onclick="event.preventDefault(); loadPage('${viewBase}html-tags.html');">HTML Tags</a>
-      <a href="${viewBase}html-div.html" onclick="event.preventDefault(); loadPage('${viewBase}html-div.html');">HTML Div</a>
-      <a href="${viewBase}test.html" onclick="event.preventDefault(); loadPage('${viewBase}test.html');">HTML Test</a>
-      <a href="${viewBase}test2.html" onclick="event.preventDefault(); loadPage('${viewBase}test2.html');">HTML Test2</a>
-    `;
+      <a href="${viewBase}about.html" onclick="event.preventDefault(); loadPage('${viewBase}1-html.about.html');">HTML Intro</a>
+      <a href="${viewBase}html-html-editor.html" onclick="event.preventDefault(); loadPage('${viewBase}2-html-html-editor.html');">HTML Editor</a>
+      <a href="${viewBase}html-basic.html" onclick="event.preventDefault(); loadPage('${viewBase}3-html-basic.html');">HTML Basic</a>
+      <a href="${viewBase}html-attributes.html" onclick="event.preventDefault(); loadPage('${viewBase}4-html-attributes.html');">HTML Attributes</a>
+      <a href="${viewBase}html-heading.html" onclick="event.preventDefault(); loadPage('${viewBase}5-html-heading.html');">HTML Heading</a>
+      <a href="${viewBase}html-paragraphs.html" onclick="event.preventDefault(); loadPage('${viewBase}6-html-paragraphs.html');">HTML Paragraphs</a>
+      <a href="${viewBase}html-style.html" onclick="event.preventDefault(); loadPage('${viewBase}7-html-style.html');">HTML Style</a>
+      <a href="${viewBase}html-text-formatting.html" onclick="event.preventDefault(); loadPage('${viewBase}8-html-text-formatting.html');">HTML Text Formatting</a>
+      <a href="${viewBase}html-quotations.html" onclick="event.preventDefault(); loadPage('${viewBase}9-html-quotations.html');">HTML Quotations</a>
 
+    `;
   }
 
   // If user clicks CSS button
   else if (type === "css") {
-
     sidebar.innerHTML = `
       <h3>CSS Topics</h3>
 
       <a href="${viewBase}css-intro.html" onclick="event.preventDefault(); loadPage('${viewBase}css-intro.html');">CSS Intro</a>
       <a href="${viewBase}css-selectors.html" onclick="event.preventDefault(); loadPage('${viewBase}css-selectors.html');">CSS Selectors</a>
     `;
-
   }
 }
-
 
 // ================= PAGE LOADER FUNCTION =================
 // This function loads different HTML files inside content area
@@ -45,7 +42,7 @@ function loadPage(page) {
   const content = document.getElementById("content");
   content.innerHTML = `
     <div class="content-card">
-      <h2>Preview</h2>
+      <h2>Welcome To New Web developer Tutorial</h2>
       <p>This panel shows the selected HTML page from the sidebar.</p>
       <iframe src="${page}" title="Page preview"></iframe>
     </div>
